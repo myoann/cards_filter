00c3a92c248a82baafc0b81c7448449355b6aac5
Yoann Moise <myoann@users.noreply.github.com>
2019-10-29 20:53:52 +0100

    Add compatibility with IE11 through react-app-polyfill and add package-lock.json

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+import 'react-app-polyfill/ie11';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import { Provider } from 'react-redux';
-
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+
 import reducers from './reducers';
 import './index.scss';
 import App from './components/';
